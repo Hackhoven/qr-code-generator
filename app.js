7117fc2c682a1a569d42b22ee3c8985d443bf919
Hackhoven <elmir.hacizade.eh@gmail.com>
2024-05-05 04:09:27 +0400

    adding toggle mode

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+function toggleTheme() {
+    var isChecked = document.getElementById("theme-switch-checkbox").checked;
+    document.body.classList.toggle("dark-theme", isChecked);
+    localStorage.setItem("darkMode", isChecked); 
+    updateToggleText(isChecked);
+}
+
+function updateToggleText(isDarkMode) {
+    var toggleText = document.getElementById("toggle-text");
+    toggleText.textContent = isDarkMode ? "Enable Light Mode" : "Enable Dark Mode";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var isChecked = localStorage.getItem("darkMode") === "true";
+    document.getElementById("theme-switch-checkbox").checked = isChecked;
+    document.body.classList.toggle("dark-theme", isChecked);
+    updateToggleText(isChecked);
+});
+
 function generateQRCode() {
     var text = document.getElementById("text-input").value;
     var qrContainer = document.getElementById("qrcode");
-    qrContainer.innerHTML = ""; 
+    qrContainer.innerHTML = "";
     
     if (text.trim() !== "") {
         new QRCode(qrContainer, {
@@ -12,14 +31,11 @@ function generateQRCode() {
             colorLight: "#ffffff",
             correctLevel: QRCode.CorrectLevel.H
         });
-
         document.getElementById("download-button").style.display = "block";
     } else {
         document.getElementById("download-button").style.display = "none";
     }
 }
-
-
 
 function downloadQRCode() {
     var canvas = document.querySelector('#qrcode canvas');
@@ -33,4 +49,3 @@ function downloadQRCode() {
         alert("No QR code available to download.");
     }
 }
-
